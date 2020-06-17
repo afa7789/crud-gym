3576d0651e5715fc4524166162866074f508df1c
ARTHUR ABEILICE <arthur.abeilice@codificar.com.br>
2020-06-16 22:43:09 -0300

    fillabels

--- database/seeds/PaymentStatusSeeder.php
+++ database/seeds/PaymentStatusSeeder.php
@@ -11,21 +11,12 @@ class PaymentStatusSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        //
-=======
->>>>>>> 11d9518d8a9151caf979c37ef78248344c059407
         $data = [
             ['name'=> "Pending" , 'created_at' => date("Y-m-d H:i:s") ],
             ['name'=> "Completed" ,'created_at' => date("Y-m-d H:i:s") ],
             ['name'=> "Ignored" , 'created_at' => date("Y-m-d H:i:s") ],
         ];
-<<<<<<< HEAD
-
-=======
         
->>>>>>> 11d9518d8a9151caf979c37ef78248344c059407
         DB::table('payment_statuses')->insert($data);
     }
-}
 }
