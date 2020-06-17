11d9518d8a9151caf979c37ef78248344c059407
ARTHUR ABEILICE <arthur.abeilice@codificar.com.br>
2020-06-16 21:20:53 -0300

    pega na minha pica e pede bença

--- database/seeds/PaymentStatusSeeder.php
+++ database/seeds/PaymentStatusSeeder.php
@@ -11,6 +11,12 @@ class PaymentStatusSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            ['name'=> "Pending"],
+            ['name'=> "Completed"],
+            ['name'=> "Ignored"],
+        ];
+        
+        DB::table('payment_statuses')->insert($data);
     }
 }
