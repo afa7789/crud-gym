48565d6e979f11e5dcbdb03903302f349c3e886f
GitScrider <igorlt96@hotmail.com>
2020-06-16 21:56:28 -0300

    Test

--- database/seeds/PaymentStatusSeeder.php
+++ database/seeds/PaymentStatusSeeder.php
@@ -12,5 +12,13 @@ class PaymentStatusSeeder extends Seeder
     public function run()
     {
         //
+        $data = [
+            ['name'=> "Pending"],
+            ['name'=> "Completed"],
+            ['name'=> "Ignored"],
+        ];
+
+        DB::table('payment_statuses')->insert($data);
     }
+}
 }
