26be44e920f7ba0efc5774a4443d58d885c7a2d0
ARTHUR ABEILICE <arthur.abeilice@codificar.com.br>
2020-06-18 20:21:43 -0300

    teste

--- public/test.php
+++ public/test.php
@@ -1,7 +1,12 @@
 <?php
 
 include(__DIR__."/../app/Classudo.php");
+include(__DIR__."/../app/Http/AdminController.php");
 
 $a = new \App\Classudo();
 $a->helloworld();
+
 // echo "hello world";
+
+$b = new \App\Http\Controllers\AdminController();
+$b->ListAdmins();
