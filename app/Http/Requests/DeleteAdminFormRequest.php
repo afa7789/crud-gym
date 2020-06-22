82e194a84e5970e2ad5d7ca32d45224bc7800fd4
ARTHUR ABEILICE <arthur.abeilice@codificar.com.br>
2020-06-21 22:21:48 -0300

    subindo mais mudança

--- app/Http/Requests/DeleteAdminFormRequest.php
+++ app/Http/Requests/DeleteAdminFormRequest.php
@@ -3,6 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
 
 class DeleteAdminFormRequest extends FormRequest
 {
@@ -23,8 +27,42 @@ class DeleteAdminFormRequest extends FormRequest
      */
     public function rules()
     {
+
         return [
-            //
+            'id' => ['required','email']
+        ];
+      
+    }
+
+    public function messages(){
+        return [
+             'email.required' => 'A Email from one admin is required.'
         ];
     }
+
+        /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation(){
+
+    }
+
+    protected function failedValidation(Validator $validator){   
+        // catch error messages     
+        $error_messages = $validator->errors()->all();
+
+        // Shown Error Parameters
+        throw new HttpResponseException(
+            response()->json(
+                [
+                    'success' => false,
+                    'error' => $error_messages[0],
+                    'error_code' => 10,##\ApiErrors::REQUEST_FAILED, #ApiErrors
+                    'error_messages' => $error_messages,
+                ]
+        ));
+    }
+
 }
